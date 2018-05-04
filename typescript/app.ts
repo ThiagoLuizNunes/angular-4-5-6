9d6d015fceb1b3581d9b50aa0c8b52fe36c570b1
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2018-05-04 19:27:27 -0300

    add config files and angular examples

---
+++ typescript/app.ts
@@ -0,0 +1,7 @@
+let message: string = 'Help me Obi-Wan Kenogi, you are my only hop!';
+
+console.log(message);
+
+let episode: number = 0;
+episode = episode + 1;
+
---
+++ typescript/dist/app.js
@@ -0,0 +1,4 @@
+var message = 'Help me Obi-Wan Kenogi, you are my only hop!';
+console.log(message);
+var episode = 0;
+episode = episode + 1;
